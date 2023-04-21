3bbee9390c543125dbc5efa73efa11394e3d5dd6
Viotra <abeltimothy1@gmail.com>
2023-04-21 10:54:22 -0600

    Completed Functions Activity

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,42 @@ using System;
 
 class Program
 {
+    static void Welcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string UserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static double FavNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        double number = double.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static double SquareNumbers(double num)
+    {
+        double squaredNum = Math.Pow(num, 2);
+        return squaredNum;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string uName;
+        double uNum;
+        double squaredNum;
+
+        Welcome();
+        uName = UserName();
+        uNum = FavNumber();
+        squaredNum = SquareNumbers(uNum);
+
+        Console.Write($"{uName}, the square of your number is {squaredNum}");       
+        
     }
 }
