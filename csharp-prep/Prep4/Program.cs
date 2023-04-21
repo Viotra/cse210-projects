604d0b5529caa0a2f6d35c958b714b2dd1edb4e0
Viotra <abeltimothy1@gmail.com>
2023-04-20 20:01:58 -0600

    Completed List assignment without stretch challenges.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int uNumber;
+
+        do
+        {
+            Console.Write("Please enter a number to add to our list. (To stop, enter 0.) ");
+            uNumber = int.Parse(Console.ReadLine());
+            
+            if (uNumber != 0)
+            {
+                numbers.Add(uNumber);
+            }
+        }
+        while (!(uNumber == 0));
+
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = numbers.Max();
+
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+
     }
 }
