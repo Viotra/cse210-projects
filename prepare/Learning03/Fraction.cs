c41dbb3d61b0ce8394eb296d8e9d10643fdd1a63
Viotra <abeltimothy1@gmail.com>
2023-05-01 18:31:09 -0600

    Finished learning activity for Fraction Class.

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,60 @@
+using System;
+
+public class Fraction{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        _numerator = 1;
+       _denominator = 1;
+    }
+
+    public Fraction (int numerator)
+    {
+        _numerator = numerator;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    public void SetNumerator()
+    {
+        Console.Write("Set a new numerator: ");
+        int numerator = int.Parse(Console.ReadLine());
+        _numerator = numerator;
+    }
+
+    public int GetNumerator()
+    {
+        return _numerator;
+    }
+
+    public void SetDenominator()
+    {
+        Console.Write("Set a new denominator: ");
+        int denominator = int.Parse(Console.ReadLine());
+        _denominator = denominator;
+    }
+
+    public int GetDenominator()
+    {
+        return _denominator;
+    }
+
+    public string GetFractionString()
+    {
+        string fractionString = $"{_numerator}/{_denominator}";
+        return fractionString;
+    }
+
+    public double GetDecimalValue()
+    {
+        double decimalValue = (double) _numerator / (double) _denominator;
+        return decimalValue;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,50 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction fraction1 = new Fraction();
+
+        string fractionString1 = fraction1.GetFractionString();
+        Console.WriteLine(fractionString1);
+
+        string decimalValue1 = fraction1.GetDecimalValue().ToString();
+        Console.WriteLine(decimalValue1);
+
+        Fraction fraction2 = new Fraction(6);
+
+        string fractionString2 = fraction2.GetFractionString();
+        Console.WriteLine(fractionString2);
+
+        string decimalValue2 = fraction2.GetDecimalValue().ToString();
+        Console.WriteLine(decimalValue2);
+
+        Fraction fraction3 = new Fraction(6,7);
+
+        string fractionString3 = fraction3.GetFractionString();
+        Console.WriteLine(fractionString3);
+
+        string decimalValue3 = fraction3.GetDecimalValue().ToString();
+        Console.WriteLine(decimalValue3);
+    
+        Fraction fraction4 = new Fraction (3,4);
+
+        string fractionString4 = fraction4.GetFractionString();
+        Console.WriteLine(fractionString4);
+
+        string decimalValue4 = fraction4.GetDecimalValue().ToString();
+        Console.WriteLine(decimalValue4);
+
+        Fraction fraction5 = new Fraction (1,3);
+
+        string fractionString5 = fraction5.GetFractionString();
+        Console.WriteLine(fractionString5);
+
+        string decimalValue5 = fraction5.GetDecimalValue().ToString();
+        Console.WriteLine(decimalValue5);
+
+        // double test = 3.0/4.0;
+        // string sTest = test.ToString();
+        // Console.WriteLine(sTest);
+
     }
 }
