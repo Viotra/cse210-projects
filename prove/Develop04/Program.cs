5317b254bbfba91882937359fd9a55800fbf35ae
Viotra <abeltimothy1@gmail.com>
2023-05-13 22:03:54 -0600

    Added comment about extra items

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,5 +1,8 @@
 using System;
 
+/*Extra items: Created a menu class to keep more information encapsulated.
+Added a few more personal animations to program and created a separate list
+of follow up questions for each reflection question.*/
 class Program
 {
     public static CountdownEvent test = new CountdownEvent(1);
