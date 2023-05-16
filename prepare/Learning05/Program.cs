80318e3fef8f0399c98d0e4b710747fa16786d98
Viotra <abeltimothy1@gmail.com>
2023-05-15 22:40:45 -0600

    Changed get area in Shape to abstract. Had abstract originally, but instructions said virtual. Example shows abstract.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -6,18 +6,28 @@ class Program
     {
         Square square = new Square("red", 4.5f);
 
-        Console.WriteLine(square.GetColor());
-        Console.WriteLine(square.GetArea());
+        // Console.WriteLine(square.GetColor());
+        // Console.WriteLine(square.GetArea());
 
         Rectangle rectangle = new Rectangle("blue", 1.87, 22.45);
 
-        Console.WriteLine(rectangle.GetColor());
-        Console.WriteLine(rectangle.GetArea());
+        // Console.WriteLine(rectangle.GetColor());
+        // Console.WriteLine(rectangle.GetArea());
 
-        Circle cirle = new Circle("yellow", 2);
+        Circle circle = new Circle("yellow", 2);
 
-        Console.WriteLine(cirle.GetColor());
-        Console.WriteLine(cirle.GetArea());
+        // Console.WriteLine(cirle.GetColor());
+        // Console.WriteLine(cirle.GetArea());
 
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
--- prepare/Learning05/Shape.cs
+++ prepare/Learning05/Shape.cs
@@ -1,4 +1,4 @@
-public class Shape
+public abstract class Shape
 {
     private string _color;
 
@@ -17,9 +17,5 @@ public class Shape
         _color = color;
     }
 
-    public virtual double GetArea()
-    {
-        double area = 2 * 2;
-        return area;
-    }
+    public abstract double GetArea();
 }
