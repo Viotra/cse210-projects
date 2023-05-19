1ed62238bfbc87688db6237eb0b103df301d386c
Viotra <abeltimothy1@gmail.com>
2023-05-18 20:30:28 -0600

    moved everything around to follow advice of Bro. Lythgoe.

--- prove/Develop05/Menu.cs
+++ prove/Develop05/Menu.cs
@@ -1,6 +1,6 @@
 class Menu
 {
-    private List<string> _menuOptions = new List<string>()
+    private List<string> _mainMenu = new List<string>()
     {
         "1. Create New Goal",
         "2. List Goals",
@@ -9,95 +9,22 @@ class Menu
         "5. Record Event",
         "6. Quit"
     };
-    private List<Goal> goals = new List<Goal>();
-    private List<string> _gMenuOptions = new List<string>()
+   
+    private List<string> _goalMenu = new List<string>()
     {
         "1. SimpleGoal  Goal",
         "2. Eternal Goal",
         "3. Checklist Goal"
     };
 
-    public void RunMenu()
+    public List<string> GetMainMenu()
     {
-        string userInput = "";
-
-        while (userInput != "6" && userInput != "quit")
-        {
-            Console.WriteLine("Menu Options:");
-
-            foreach (string option in _menuOptions)
-            {
-                Console.WriteLine(option);
-            }
-
-            userInput = Console.ReadLine().ToLower();
-
-            switch (userInput)
-            {
-                case "1":
-                case "create new goal":
-                    RunGoalMenu();
-                    break;
-                case "2":
-                case "list goals":
-                    foreach (Goal goal in goals)
-                    {
-                        Console.WriteLine(goal.GetGoal());
-                    }
-                    break;
-                case "3":
-                case "save goals":
-                    break;
-                case "4":
-                case "load goals":
-                    break;
-                case "5":
-                case "record event":
-                    break;
-                // case "6":
-                // case "quit":
-                //     break;
-            }
-        }
+        return _mainMenu;
     }
 
-    private void RunGoalMenu()
+    public List<string> GetGoalMenu()
     {
-        string userInput = "";
-
-        Console.Clear();
-        Console.WriteLine("Choose from one of the following goals:");
-
-        foreach (string goal in _gMenuOptions)
-        {
-            Console.WriteLine(goal);
-        }
-
-        Console.Write("Goal #");
-        userInput = Console.ReadLine();
-
-        
-
-        switch(userInput)
-        {
-            case "1":
-                //SimpleGoal  Goal
-                SimpleGoal  simple = new SimpleGoal ();
-                simple.Test(1);
-                goals.Add(simple);
-                break;
-            case "2":
-                //Eternal Goal
-                Eternal eternal = new Eternal();
-                eternal.Test(2);
-                break;
-            case "3":
-                //Checklist Goal
-
-                Checklist checklist = new Checklist();
-                checklist.Test(3);
-                break;
-        }
+        return _goalMenu;
     }
 
     
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,9 +4,82 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        List<Goal> goals = new List<Goal>();
 
         Menu menu = new Menu();
-        menu.RunMenu();
+        List<string> mainMenu = menu.GetMainMenu();
+ 
+        string userInput = "";
+
+        while (userInput != "6" && userInput != "quit")
+        {
+            Console.WriteLine("Menu Options:");
+
+            foreach (string option in mainMenu)
+            {
+                Console.WriteLine(option);
+            }
+
+            userInput = Console.ReadLine().ToLower();
+
+            switch (userInput)
+            {
+                case "1":
+                case "create new goal":
+
+                    List<string> goalMenu = menu.GetGoalMenu();
+                    Console.Clear();
+                    Console.WriteLine("Choose from one of the following goals:");
+
+                    foreach (string option in goalMenu)
+                    {
+                        Console.WriteLine(option);
+                    }
+
+                    Console.Write("Goal #");
+                    userInput = Console.ReadLine();      
+
+                    switch(userInput)
+                    {
+                        case "1":
+                            //SimpleGoal  Goal
+                            SimpleGoal  simple = new SimpleGoal ();
+                            simple.Test(1);
+                            goals.Add(simple);
+                            break;
+                        case "2":
+                            //Eternal Goal
+                            Eternal eternal = new Eternal();
+                            eternal.Test(2);
+                            break;
+                        case "3":
+                            //Checklist Goal
+
+                            Checklist checklist = new Checklist();
+                            checklist.Test(3);
+                            break;
+                    }
+                    break;
+                case "2":
+                case "list goals":
+                    foreach (Goal goal in goals)
+                    {
+                        Console.WriteLine(goal.GetGoal());
+                    }
+                    break;
+                case "3":
+                case "save goals":
+                    break;
+                case "4":
+                case "load goals":
+                    break;
+                case "5":
+                case "record event":
+                    break;
+                // case "6":
+                // case "quit":
+                //     break;
+            }
+        }       
     }
-}
+} 
